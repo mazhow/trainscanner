4fffe56264da54fee319e7e62f8775faa82a1922
Marie Howard <marie.cockburn@gmail.com>
2019-11-13 16:49:22 +0000

    Fixed gradle issues and created API controller

--- demo/src/main/java/com/marie/trainscanner/TrainScannerApplication.java
+++ demo/src/main/java/com/marie/trainscanner/TrainScannerApplication.java
@@ -1,13 +1,13 @@
-package com.marie.demo;
+package com.marie.trainscanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class TrainScannerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(TrainScannerApplication.class, args);
 	}
 
 }
---
+++ demo/src/test/java/com/marie/trainscanner/TrainScannerControllerScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marie.trainscanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainScannerControllerScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
